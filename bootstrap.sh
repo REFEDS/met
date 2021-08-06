450518b5be48f12503c4b52a1c9b4c126d9dc4c2
Manuel Saelices <msaelices@gmail.com>
2021-08-06 10:15:46 +0200

    chore (vagrant): Fix some vagrant configuration for using mysql as the default DB

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,7 +17,9 @@ apt-get -y install gettext
 apt-get -y install memcached
 apt-get -y install python3
 apt-get -y install libmysqlclient-dev
-apt-get -y install python3-venv virtualenvwrapper
+apt-get -y install mysql-server mysql-client
+apt-get -y install python3-mysqldb
+apt-get -y install python3-virtualenv
 apt-get -y install python3-pip
 
 # Postgresql 10
@@ -40,15 +42,13 @@ sudo -u postgres createdb --owner=met --encoding=UTF8 met
 
 echo "export LC_ALL='en_US.UTF-8'" >> /home/vagrant/.profile
 echo "export LC_CTYPE='en_US.UTF-8'" >> /home/vagrant/.profile
-echo "export WORKON_HOME=~/Envs" >> /home/vagrant/.profile
-echo "source /usr/share/virtualenvwrapper/virtualenvwrapper.sh" >> /home/vagrant/.profile
 echo "cd /vagrant/" >> /home/vagrant/.profile
-echo "workon met-env" >> /home/vagrant/.profile
+echo "source ~/venvs/met/bin/activate" >> /home/vagrant/.profile
 echo "export PATH=$PATH:/home/vagrant/.local/bin/"
 
 su vagrant << EOF
-python3 -m virtualenv -p /usr/bin/python3 ~/Envs/met-env
-source ~/Envs/met-env/bin/activate
+virtualenv --python=python3 ~/venvs/met
+source ~/venvs/met/bin/activate
 cd /vagrant
 pip install -r requirements.txt
 EOF
