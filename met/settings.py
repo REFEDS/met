309bc27565fff563b756cd0cb7c8532ae05061b4
Manuel Saelices <msaelices@gmail.com>
2021-08-13 11:24:39 +0200

    fix (static): It's a bad design pattern to have the STATIC_ROOT being one of the STATICFILES_DIRS directories

--- met/settings.py
+++ met/settings.py
@@ -93,7 +93,7 @@ MEDIA_URL = '%s/media/' % BASEURL
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(BASEDIR, 'met', 'static')
+STATIC_ROOT = os.path.join(BASEDIR, 'met', 'static_root')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
